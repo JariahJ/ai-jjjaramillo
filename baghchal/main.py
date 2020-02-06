9802605947f9cc753f6dd171c8a7f8109cbe8435
jariah jaramillo <jariahjaramillo@yahoo.com>
2020-02-06 14:01:20 -0700

    baghchal added

---
+++ baghchal/main.py
@@ -0,0 +1,13 @@
+from hungrytigeragent import HungryTigerAgent
+from game import Game
+import random
+from matchup import Matchup
+from hungrytigeragent import HungryTigerAgent
+from stats import Stats
+
+matchup = Matchup()
+# matchup.tigerAgent = HungryTigerAgent(matchup.game)
+matchup.goatAgent = sideHuggingGoat(matchup.game)
+stats = Stats(matchup, 1000)
+stats.playAll()
+stats.summarize()
