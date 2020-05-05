8c8e9928985f223bcb4d58f13cc2437cba44a870
jariah <jariahjaramillo@yahoo.com>
2020-05-05 17:57:13 -0600

    stem words and remove dups

--- finalproject/main.py
+++ finalproject/main.py
@@ -1,9 +1,10 @@
 import nltk
+nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
+
 stemmer = LancasterStemmer()
 
 import numpy as np
-import tflearn
 import tensorflow as tf
 import random
 
@@ -15,16 +16,25 @@ with open('intents.json') as json_data:
 # organization using natural language processor 
 words = []
 classes = []
-document = []
+documents = []
 words_to_ignore = ['?']
 
 for intent in intents['intents']:
-	for pattern in intent['patterns']:
-	list_of_words = nltk.word_tokenize(pattern)
-	words.add(word)
-	documents.append((list_of_words, intent['tag']))
+    for pattern in intent['patterns']:
+        list_of_words = nltk.word_tokenize(pattern)
+        words.extend(list_of_words)
+        documents.append((list_of_words, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
-	if intent['tag'] not in classes:
-		classes.append(intent['tag'])
+words = [stemmer.stem(w.lower()) for w in words if w not in words_to_ignore]
+
+words = sorted(list(set(words)))
+
+classes = sorted(list(set(classes)))
+
+print(len(documents), "documents")
+print(len(classes), "classes", classes)
+print(len(words), "unique stemmed words", words)
 
 
