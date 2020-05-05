441fedbd5ffdd3818724ae1ee523b15be695a243
brandon story <brandonmichaelstory@gail.com>
2020-05-05 16:55:12 -0600

    added loops to tokenize words

--- finalproject/main.py
+++ finalproject/main.py
@@ -7,3 +7,24 @@ import tflearn
 import tensorflow as tf
 import random
 
+import json
+with open('intents.json') as json_data:
+	intents = json.load(json_data)
+
+
+# organization using natural language processor 
+words = []
+classes = []
+document = []
+words_to_ignore = ['?']
+
+for intent in intents['intents']:
+	for pattern in intent['patterns']:
+	list_of_words = nltk.word_tokenize(pattern)
+	words.add(word)
+	documents.append((list_of_words, intent['tag']))
+
+	if intent['tag'] not in classes:
+		classes.append(intent['tag'])
+
+
